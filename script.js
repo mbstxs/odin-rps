8392e8b698dbe5bb0b4ffd3ad9def72c3e738b31
Marcelo Atkins <xxatkinsxx@gmail.com>
2025-08-06 15:54:38 +0100

    fix(script):better responses

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const options = Array("paper", "scissors", "rock");
-let humanScore = 0;
-let computerScore = 0;
 
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3);
@@ -15,42 +13,51 @@ const getHumanChoice = () => {
         return options[humanChoice - 1];
 }
 
-const getScores = () => {
+const getScores = (humanScore, computerScore) => {
     console.log("-Scores-");
     console.log("human: ", humanScore);
     console.log("computer: ", computerScore);
 };
 
-const getResult = (humanChoice, computerChoice) => {
+const getResult = (humanChoice, computerChoice, humanScore, computerScore) => {
     if (humanChoice == computerChoice)
         return console.log("tie");
     if (humanChoice == "paper" && computerChoice == "rock") {
         humanScore += 1;
-        return console.log("User Wins");
+        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
     }
     if (humanChoice == "scissors" && computerChoice == "paper") {
         humanScore += 1;
-        return console.log("User Wins");
+        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
     }
     if (humanChoice == "rock" && computerChoice == "scissors") {
         humanScore += 1;
-        return console.log("User Wins");
+        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
     }
     else {
-        computerScore += 1;
-        return console.log("Computer Wins");
+        computerScore = computerScore + 1;
+        return console.log(`You Lose, ${computerChoice} beats ${humanChoice}`);
     }
 };
 
-function playRound(humanChoice, computerChoice) {
-    console.log("welcome to Odin's RPS!\n");
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
     console.log(`computer choice:  ${computerChoice}`);
     console.log(`user choice:  ${humanChoice}`);
-    getResult(humanChoice, computerChoice);
-    getScores();
+    getResult(humanChoice, computerChoice, humanScore, computerScore);
+    getScores(humanScore, computerScore);
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
+    console.log("welcome to Odin's RPS!\n");
+
+    // for (i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection, humanScore, computerScore);
+    // }
+}
+
+playGame()
