9ae985cafd1803dbadc28eb8f0a9da9510e72c51
Marcelo Atkins <xxatkinsxx@gmail.com>
2025-08-06 18:14:20 +0100

    fix(script): major refactor + working solution

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getComputerChoice = () => {
 };
 
 const getHumanChoice = () => {
-    let humanChoice = prompt("make your pick [1-3]");
+    let humanChoice = prompt("make your pick. [1-3]");
     if (isNaN(humanChoice) || humanChoice === 0)
         return console.log("invalid choice");
     else
@@ -14,50 +14,79 @@ const getHumanChoice = () => {
 }
 
 const getScores = (humanScore, computerScore) => {
+    console.log("")
     console.log("-Scores-");
     console.log("human: ", humanScore);
     console.log("computer: ", computerScore);
+    console.log("");
 };
 
-const getResult = (humanChoice, computerChoice, humanScore, computerScore) => {
-    if (humanChoice == computerChoice)
-        return console.log("tie");
-    if (humanChoice == "paper" && computerChoice == "rock") {
-        humanScore += 1;
-        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
+const getResult = (humanChoice, computerChoice) => {
+    if (humanChoice == computerChoice) {
+        console.log("tie");
+        return 2
     }
-    if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore += 1;
-        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
-    }
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-        humanScore += 1;
-        return console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
-    }
-    else {
-        computerScore = computerScore + 1;
-        return console.log(`You Lose, ${computerChoice} beats ${humanChoice}`);
+
+    switch (humanChoice) {
+        case "paper":
+            if (computerChoice == "rock") {
+                console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
+                return 0
+            } else if (computerChoice == "scissors") {
+                console.log(`You Lose, ${computerChoice} beats ${humanChoice}`);
+                return 1
+            }
+            break;
+        case "rock":
+            if (computerChoice == "scissors") {
+                console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
+                return 0
+            } else if (computerChoice == "paper") {
+                console.log(`You Lose, ${computerChoice} beats ${humanChoice}`);
+                return 1
+            }
+
+            break;
+        case "scissors":
+            if (computerChoice == "paper") {
+                console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
+                return 0
+            } else if (computerChoice == "rock") {
+                console.log(`You Lose, ${computerChoice} beats ${humanChoice}`);
+                return 1
+            }
+            break;
+        default:
+            console.log("Ivalid choice");
+            break;
     }
 };
 
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
-    console.log(`computer choice:  ${computerChoice}`);
-    console.log(`user choice:  ${humanChoice}`);
-    getResult(humanChoice, computerChoice, humanScore, computerScore);
-    getScores(humanScore, computerScore);
+function playRound(humanChoice, computerChoice) {
+    console.log("round ", i + 1);
+    let result = getResult(humanChoice, computerChoice);
+    console.log("---------------");
+    return result;
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    console.log("welcome to Odin's RPS!\n");
+    console.log("welcome to Odin's RPS!");
+    console.log('');
 
-    // for (i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection, humanScore, computerScore);
-    // }
+    for (i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        let result = playRound(humanSelection, computerSelection)
+        if (result == 0)
+            humanScore += 1
+        else if (result == 1)
+            computerScore += 1;
+    }
+
+    getScores(humanScore, computerScore);
 }
 
 playGame()
